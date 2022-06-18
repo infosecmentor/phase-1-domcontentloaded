db6ae293e3d08c4d26ecc074469866c387645b4b
infosecmentor <muhambiphares@gmail.com>
2022-06-18 18:10:40 +0300

    commit tree fc96f1f1ccb1286ca8b554f806eb35d233568e7e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let newLine = document.getElementById("text");
+    newLine.textContent = "This is really cool!"
+});
